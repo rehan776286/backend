755491dc5559a8a44aea42a722b65b99b04e767d
rehan776286 <122428687+rehan776286@users.noreply.github.com>
2025-05-12 23:22:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ import cors from "cors";
 import cookieparser from "cookie-parser";
 
 const app = express();
+import cors from 'cors';
+app.use(cors({
+  origin: 'https://frontend-61n5.onrender.com', // your frontend URL on Render or elsewhere
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
-app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieparser());
