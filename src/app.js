8621f903e6e4c7366aa070b566b46436a7fa50bd
Rehan Khan <rehankhan2mrk@gmail.com>
2025-05-16 15:13:46 +0530

    add vercel

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ import cors from "cors";
 import cookieparser from "cookie-parser";
 
 const app = express();
-app.use(cors({
-  origin: ['https://frontend-61n5.onrender.com','http://localhost:5173'], // your frontend URL on Render or elsewhere
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "https://frontend-61n5.onrender.com",
+      "http://localhost:5173",
+      "https://frontend-7e94.vercel.app/",
+    ], // your frontend URL on Render or elsewhere
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
